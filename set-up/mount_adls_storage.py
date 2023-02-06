2c7e5103e03ab451f875c60221c1e0951adbbef6
hdh997 <hdh997>
2023-02-06 08:05:49 +0000

    initiate
    mounting datalakes set up

---
+++ set-up/mount_adls_storage.py
@@ -0,0 +1,54 @@
+# Databricks notebook source
+dbutils.secrets.help()
+
+# COMMAND ----------
+
+dbutils.secrets.list('formula1-scope')
+
+# COMMAND ----------
+
+storage_account_name = "myformula1projectdl"
+client_id = dbutils.secrets.get(scope="formula1-scope", key="databricks-client-id")
+tenant_id = dbutils.secrets.get(scope="formula1-scope", key="databricks-tenant-id")
+client_secret = dbutils.secrets.get(scope="formula1-scope", key="databricks-client-secret")
+
+# COMMAND ----------
+
+configs = {"fs.azure.account.auth.type": "OAuth",
+           "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+           "fs.azure.account.oauth2.client.id": f"{client_id}",
+           "fs.azure.account.oauth2.client.secret": f"{client_secret}",
+           "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
+
+# COMMAND ----------
+
+def mount_adls(container_name):
+    dbutils.fs.mount(
+      source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+      mount_point = f"/mnt/{storage_account_name}/{container_name}",
+      extra_configs = configs)
+
+# COMMAND ----------
+
+mount_adls("raw")
+
+# COMMAND ----------
+
+mount_adls("processed")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/mnt/myformula1projectdl/processed")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/mnt/myformula1projectdl/raw")
+
+# COMMAND ----------
+
+#dbutils.fs.unmount("/mnt/myformula1projectdl/processed")
+#dbutils.fs.unmount("/mnt/myformula1projectdl/raw")
+
+# COMMAND ----------
+
+
