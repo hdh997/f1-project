927c411d8f5e1d15b021c306e19383a760517bfb
hdh997 <hdh997>
2023-02-13 00:51:20 +0000

    adjust name and add 1 more column

--- transformation/1.race_results.py
+++ transformation/1.race_results.py
@@ -79,7 +79,7 @@ from pyspark.sql.functions import current_timestamp
 
 # COMMAND ----------
 
-final_df = race_results_df.select("race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality", "team", "grid", "fastest_lap", "race_time", "points")\
+final_df = race_results_df.select("race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality", "team", "grid", "fastest_lap", "race_time", "points","position")\
 .withColumn("created_time", current_timestamp())
 
 # COMMAND ----------
