6e19364e6c940fbc918f4b028efe932ef3fbf652
hdh997 <hdh997>
2023-03-06 02:54:01 +0000

    updated

--- transformation/4.calculate_race_results.py
+++ transformation/4.calculate_race_results.py
@@ -33,15 +33,16 @@ spark.sql(f"""
         AS
         SELECT races.race_year,
               constructors.name AS team_name,
+              drivers.driver_id,
               drivers.name AS driver_name,
               races.race_id,
               results.position,
               results.points,
               11 - results.position AS calculated_points
-        FROM results
-        JOIN drivers ON (results.driver_id = drivers.driver_id)
-        JOIN constructors ON (results.constructor_id = constructors.constructor_id)
-        JOIN races ON (results.race_id = races.race_id)
+        FROM f1_processed.results
+        JOIN f1_processed.drivers ON (results.driver_id = drivers.driver_id)
+        JOIN f1_processed.constructors ON (results.constructor_id = constructors.constructor_id)
+        JOIN f1_processed.races ON (results.race_id = races.race_id)
         WHERE results.position <= 10
           AND results.file_date = '{v_file_date}'
 """)
@@ -51,17 +52,22 @@ spark.sql(f"""
 # MAGIC %sql
 # MAGIC MERGE INTO  f1_performance.calculated_race_results tgt
 # MAGIC USING race_results_updated upd
-# MAGIC ON (tgt.driver_id = udp.driver_id AND tgt.race_id = udp.race_id)
+# MAGIC ON (tgt.driver_id = upd.driver_id AND tgt.race_id = upd.race_id)
 # MAGIC WHEN MATCHED THEN
-# MAGIC   UPDATE SET tgt.position = udp.position,
-# MAGIC             tgt.points = udp.points,
-# MAGIC             tgt.calculated_points = udp.calculated_points,
+# MAGIC   UPDATE SET tgt.position = upd.position,
+# MAGIC             tgt.points = upd.points,
+# MAGIC             tgt.calculated_points = upd.calculated_points,
 # MAGIC             tgt.updated_date = CURRENT_TIMESTAMP
 # MAGIC   WHEN NOT MATCHED
-# MAGIC      THEN INSERT (race_year, team_name, driver_id, race_id, position, points, calculated_points ,calculated_date) 
+# MAGIC      THEN INSERT (race_year, team_name, driver_id, race_id, position, points, calculated_points ,created_date) 
 # MAGIC           VALUES (race_year, team_name, driver_id, race_id, position, points, calculated_points ,CURRENT_TIMESTAMP)
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT * FROM f1_performance.calculated_race_results;
+# MAGIC SELECT COUNT(*) FROM race_results_updated;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_performance.calculated_race_results
